3edac096c78380c025d2460bffeeabc067e4d142
AnonTh0r <AnonTh0r@github.com>
2025-03-07 00:04:08 +0800

    delete old version

--- publisher/build/british_system/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/navi/learn_ros2/publisher_3.3/install/british_system'
--- publisher/build/british_system/share/british_system/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/navi/learn_ros2/publisher_3.3/build/british_system"
